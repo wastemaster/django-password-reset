5f14bcfee525eb4d29b5187c858794d86271bf7c
Евгений <wastemaster@gmail.com>
2022-12-20 13:54:22 +0100

    Update urls.py

--- password_reset/urls.py
+++ password_reset/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 
 urlpatterns = [
-    url(r'^recover/(?P<signature>.+)/$', views.recover_done,
+    re_path(r'^recover/(?P<signature>.+)/$', views.recover_done,
         name='password_reset_sent'),
-    url(r'^recover/$', views.recover, name='password_reset_recover'),
-    url(r'^reset/done/$', views.reset_done, name='password_reset_done'),
-    url(r'^reset/(?P<token>[\w:-]+)/$', views.reset,
+    re_path(r'^recover/$', views.recover, name='password_reset_recover'),
+    re_path(r'^reset/done/$', views.reset_done, name='password_reset_done'),
+    re_path(r'^reset/(?P<token>[\w:-]+)/$', views.reset,
         name='password_reset_reset'),
 ]
