5fc9a9e253dca5dbe0596cde836f37ebfac23c98
Евгений <wastemaster@gmail.com>
2022-12-20 13:55:27 +0100

    Update signals.py

--- password_reset/signals.py
+++ password_reset/signals.py
@@ -1,6 +1,4 @@
 from django.dispatch import Signal
 
 # signal sent when users successfully recover their passwords
-user_recovers_password = Signal(
-    providing_args=['user', 'request']
-)
+user_recovers_password = Signal()
